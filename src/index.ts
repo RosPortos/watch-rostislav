d590d98e9b60446cd6cc62fc4157c0a00b6bbee2
Your Name <you@example.com>
2023-09-24 13:42:30 +0300

    add costom

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,17 @@ import {
     DiamondPlugin,
     Vector3, GammaCorrectionPlugin, MeshBasicMaterial2, Color, AssetImporter, CameraViewPlugin, ScrollableCameraViewPlugin
 } from "webgi";
-
 import "./css/style.min.css";
-
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Lenis from '@studio-freight/lenis'
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.defaults({
+    ease: "power3.inOut",
+    duration: 1,
+});
 
 const lenis = new Lenis({
     duration: 4,
@@ -38,8 +43,6 @@ function raf(time: number) {
 }
 
 requestAnimationFrame(raf)
-
-gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", function() {
 
@@ -63,10 +66,6 @@ document.addEventListener("DOMContentLoaded", function() {
         await viewer.addPlugin(CameraViewPlugin)
         const scroller = await viewer.addPlugin(ScrollableCameraViewPlugin)
 
-        const camViewPlugin = viewer.getPlugin(CameraViewPlugin)
-
-        let cameraView = (camViewPlugin as any)._cameraViews
-
         // Loader
         const importer = manager.importer as AssetImporter
 
@@ -78,8 +77,6 @@ document.addEventListener("DOMContentLoaded", function() {
         importer.addEventListener("onLoad", (ev) => {
             gsap.to('.loader', {x: '100%', duration: 0.8, ease: 'power4.inOut', delay: 1, onComplete: () =>{
                 lenis.start()
-                /* cameraView[1].position = new Vector3(4.10, -2.02, 4.96)
-                cameraView[1].target = new Vector3(-0.05, -0.07, 0.17) */
             }})
         })
 
@@ -88,7 +85,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
         viewer.renderer.refreshPipeline()
 
-        await viewer.load("./assets/watch.glb")
+        await manager.addFromPath("./assets/watch.glb")
+
         let needsUpdate = true
 
         onUpdate()
@@ -100,33 +98,94 @@ document.addEventListener("DOMContentLoaded", function() {
 
         viewer.addEventListener('preFrame', () =>{
             if(needsUpdate){
-                camera.positionTargetUpdated(true)
+                camera.positionUpdated(true);
+                camera.targetUpdated(true);
                 needsUpdate = false
             }
         })
 
-        const exploreView = document.querySelector(".explore") as HTMLElement | null;
-        const canvasView = document.getElementById("webgi-canvas") as HTMLElement | null;
-        const canvasContainer = document.getElementById("webgi-canvas-container") as HTMLElement | null;
-        const header = document.querySelector(".header") as HTMLElement | null;
-        const exitContainer = document.querySelector(".exit--container") as HTMLElement | null;
-        const bodyButton = document.querySelector(".button--body") as HTMLElement | null;
+        const exploreView = document.querySelector(".explore") as HTMLElement;
+        const canvasView = document.getElementById("webgi-canvas") as HTMLElement;
+        const canvasContainer = document.getElementById("webgi-canvas-container") as HTMLElement;
+        const customizeBottom = document.querySelector(".customize-bottom") as HTMLElement;
+        const customizebuttonStart = document.querySelector(".explore__btn") as HTMLElement;
+        const btnStyleSilver = document.querySelector("#style-silver") as HTMLElement;
+        const btnStyleGold = document.querySelector("#style-gold") as HTMLElement;
+        const watchMaterialMain = manager.materials!.findMaterialsByName('meetal')[0] as MeshBasicMaterial2
+        const watchMaterialChrome = manager.materials!.findMaterialsByName('chrome')[0] as MeshBasicMaterial2
+        const watchMaterial2 = manager.materials!.findMaterialsByName('meetal.002')[0] as MeshBasicMaterial2
+        const watchMaterial3 = manager.materials!.findMaterialsByName('meetal.001')[0] as MeshBasicMaterial2
+
+        let cameraViews = viewer.getPlugin(CameraViewPlugin);
+        let view = (cameraViews as any)._cameraViews;
+
+        function changeStyle(style: string, e: any = null) {
+            watchMaterialMain.color = new Color(style);
+            watchMaterialChrome.color = new Color(style);
+            watchMaterial2.color = new Color(style);
+            watchMaterial3.color = new Color(style);
+            document.querySelectorAll(".customize-nav__item")?.forEach((item) => {
+                item.classList.remove("active");
+            });
+            e?.target.classList.add("active");
+            onUpdate();
+        }
+
+        // STYLES EVENT
+        btnStyleSilver?.addEventListener("click", (e) => {
+            changeStyle("#e4e5e7", e);
+        });
+
+        btnStyleGold?.addEventListener("click", (e) => {
+            changeStyle("#f7d78c", e);
+        });
 
         // EXPLORE ALL FEATURES EVENT
-        document.querySelector(".button-explore")?.addEventListener("click", () => {
-            exploreView?.style.setProperty('pointer-events', "none");
-            exitContainer?.style.setProperty('opacity', "100");
-            exitContainer?.style.setProperty('display', "flex");
-            canvasView?.style.setProperty('pointer-events', "all");
+        document.querySelector(".explore__btn")?.addEventListener("click", () => {
+            exploreView.style.setProperty('pointer-events', "none");
+            customizebuttonStart.style.setProperty('pointer-events', "none");
+            canvasView.style.setProperty('pointer-events', "all");
+            customizeBottom?.style.setProperty('pointer-events', "all");
             canvasContainer?.style.setProperty('z-index', "1");
-            header?.style.setProperty('position', "fixed");
-            document.body.style.overflowY = "hidden";
             document.body.style.cursor = "grab";
             scroller.enabled = false;
 
-            // Assuming CameraViewPlugin type exists
-            let cameraViews = viewer.getPlugin(CameraViewPlugin);
-            cameraViews?.animateToView(cameraViews.camViews[4] ?? null);
+            view[3].position = new Vector3(11.64, 2.98, 0.17)
+            view[3].target = new Vector3(-0.11, -0.00, -0.00)
+
+            lenis.stop()
+
+            gsap.to('.explore__text', {x: '-100vw'})
+            gsap.to('.customize-nav', {right: 0})
+            gsap.to('.explore__btn', {yPercent: 110})
+            gsap.to('.customize-bottom', {opacity: 1, delay: 1})
+            gsap.to('.explore__bottom--text', {opacity: 1, delay: 1.2})
+
+            cameraViews?.animateToView(view[3]);
+        });
+
+        document.querySelector(".explore__btn--exit")?.addEventListener("click", () => {
+            exploreView.style.pointerEvents = "all";
+            customizebuttonStart.style.pointerEvents = "all";
+            canvasView.style.pointerEvents = "none";
+            customizeBottom.style.pointerEvents = "none";
+            canvasContainer.style.zIndex = "unset";
+            document.body.style.cursor = "auto";
+
+            view[3].position = new Vector3(7.248, 2.554, -6.159)
+            view[3].target = new Vector3(1.443, -0.376, 1.159)
+
+            lenis.start()
+
+            gsap.to('.explore__text', {x: '0'})
+            gsap.to('.customize-nav', {right: '-100%'})
+            gsap.to('.customize-bottom', {opacity: 0})
+            gsap.to('.explore__bottom--text', {opacity: 0})
+            gsap.to('.explore__btn', {yPercent: 0, delay: 1})
+
+            cameraViews?.animateToView(view[3]);
+
+            scroller.enabled = true;
         });
 
     }
